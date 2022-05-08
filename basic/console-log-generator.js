6f0ecc217d982a057f7ea5341e57c0bb3137ef9b
JamesVonThere <jc6614522@gmail.com>
2022-05-08 15:26:50 +0800

    added console log generator

---
+++ basic/console-log-generator.js
@@ -0,0 +1,6 @@
+for(let i = 1; i <= 100; i++){
+    console.log(`console.log(\"${i}-----------------------------------------------------------------------------------------\")`);
+    console.log(`console.log(sum(${i}))`);
+    console.log(`console.log(sum2(${i}))`);
+    console.log(`console.log(sum(${i}))`);
+}
