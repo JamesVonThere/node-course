77c5caf35cdc9c28547c631fafb171ac86e6d85f
JamesVonThere <jc6614522@gmail.com>
2022-05-08 15:13:18 +0800

    added reduce

--- basic/sum.js
+++ basic/sum.js
@@ -1,3 +1,31 @@
 let sum = (n) => (n + 1) * (n / 2);
 console.log(sum(100));
 console.log(sum(3));
+
+
+
+
+function sum2(n){
+const array = [];
+let num = n;
+for(let i = 1; i < num; i++){
+    array.push(i);
+}
+const sum = array.reduce((previousValue, currentValue) => previousValue + currentValue,
+n
+);
+return sum;
+}
+
+
+console.log(sum2(100));
+console.log(sum2(3));
+
+
+let sum3 = (n) => {
+    let sum = 0;
+    for(let i = 1; i < n; i++){
+        sum += i;
+    }
+    return sum;
+}
