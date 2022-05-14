610b80c2479a684f0ac5388fc31517819db2acb6
JamesVonThere <jc6614522@gmail.com>
2022-05-14 15:28:15 +0800

    used additionTimer

--- basic/callback1.js
+++ basic/callback1.js
@@ -2,8 +2,7 @@ let dt = new Date();
 console.log(`起床了 at ${dt.toISOString()}`);
 let addTime=0;
 let doWork = function (job, timer, cb) {
-    timer += addTime;
-    addTime = timer;
+    timer = additionTimer(timer)
   setTimeout(() => {
     let dt = new Date();
     let result = `完成工作: ${job} at ${dt.toISOString()}`;
@@ -17,8 +16,8 @@ let doWork = function (job, timer, cb) {
   console.log(`在 setTimeout 之後 ${job}`);
 };
 let additionTimer = function(timer){
-    timer += addTimer;
-    addTimer = timer;
+    timer += addTime;
+    addTime = timer;
     return timer;
 }
 // 刷牙 (3000) -> 吃早餐 (5000) -> 寫功課 (3000)
