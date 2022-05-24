3930a3951337fb6e7e0118702f227ad9d823ffc7
JamesVonThere <jc6614522@gmail.com>
2022-05-24 10:59:17 +0800

    express

---
+++ simple-express/express.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (request, response, next) => {
+    response.send("<h1>Gådu?</h1>");
+});
+
+app.get('/about', (request, response, next) => {
+    response.send("<h1>du gå</h1>");
+})
+
+app.listen(3001, () => {
+    console.log('start server at port 3001');
+})
